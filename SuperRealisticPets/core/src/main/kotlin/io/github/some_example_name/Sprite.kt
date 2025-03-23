fef1a22718ed0b65358bc3dcca5997ba08573619
Adrian Dahlen Haugen <adriandh@stud.ntnu.no>
2025-03-23 22:37:38 +0100

    fix: sprite and items class

---
+++ SuperRealisticPets/core/src/main/kotlin/io/github/some_example_name/Sprite.kt
@@ -0,0 +1,28 @@
+package io.github.some_example_name.sprite
+
+import io.github.some_example_name.item.Item
+
+open class Sprite(
+    val name: String,
+    var attack: Int,
+    var health: Int,
+    var tier: Int,
+    var item: Item? = null
+) {
+    open fun onTurnStart() {
+        item?.onTurnStart(this)
+    }
+
+    open fun onBattleStart() {
+        item?.onBattleStart(this)
+    }
+
+    open fun attack(target: Sprite) {
+        println("$name attacks ${target.name}!")
+        target.health -= this.attack
+        this.health -= target.attack
+        item?.onAttack(this, target)
+    }
+
+    fun isAlive(): Boolean = health > 0
+}
