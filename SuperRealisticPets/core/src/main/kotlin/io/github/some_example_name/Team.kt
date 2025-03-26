50dcf52d82574dd9fd7b45ff85574ae0857fa87b
ber <bernardowiz65@gmail.com>
2025-03-26 15:31:45 -0300

    changing Teams.kt name

--- SuperRealisticPets/core/src/main/kotlin/io/github/some_example_name/Team.kt
+++ SuperRealisticPets/core/src/main/kotlin/io/github/some_example_name/Team.kt
@@ -1,7 +1,5 @@
 package io.github.some_example_name
 
-import io.github.some_example_name.sprite.Sprite
-
-class Teams {
+class Team {
     private val teams: MutableList<Sprite> = mutableListOf()
 }
