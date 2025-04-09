6cf5423653d1115e9a1a58d0bbfe98f804e49274
ber <bernardowiz65@gmail.com>
2025-04-09 13:06:24 -0300

    feat: Add basic game loop and player actions
    - Implemented a basic game loop in `Main.kt` with turn-based actions for players.
    - Introduced `PlayerController` to handle player actions like buying.
    - Created a basic interaction with the user to buy pets from the shop.
    - Added new animals in `sprites.json` file.
    - Updated the `gradle` cache.
    - Update the java version in `misc.xml` file.
    - Updated the `executionHistory` and `buildOutputCleanup` files.

--- SuperRealisticPets/core/src/main/kotlin/io/github/some_example_name/Main.kt
+++ SuperRealisticPets/core/src/main/kotlin/io/github/some_example_name/Main.kt
@@ -4,3 +4,44 @@ import com.badlogic.gdx.ApplicationAdapter
 
 /** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms. */
 class Main : ApplicationAdapter()
+
+fun main() {
+
+    val playerA = Player()
+    val playerB = Player()
+
+    val playerAController = PlayerController(playerA)
+    val playerBController = PlayerController(playerB)
+
+    playerAController.startTurn()
+    playerBController.startTurn()
+
+    while (true) {
+
+        println("Player A's turn")
+
+        while ( playerA.gold > 0 ) {
+
+            println("Gold : ${playerA.gold}")
+            println("Team : ${playerA.team.teams}")
+            println("Shop : ${playerA.shop.slots}")
+            println("Shop frozen : ${playerA.shop.frozenUnits}")
+            println("Chose a number between 0 to 4 to buy an animal from the shop")
+            println("Chose 5 or 6 to buy an item from the shop")
+
+            var input = readlnOrNull()?.toIntOrNull()
+
+            if (input == null) {
+                println("Invalid input. Please enter a number.")
+                continue
+            } else if (input == -1) {
+                playerAController.endTurn()
+                break
+            } else {
+                playerAController.buy(input, 0)
+            }
+        }
+
+    }
+
+}
